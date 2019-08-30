b8e89aca3aa7f96ac40fa4855c4e46c48365cd4d
Juiser1997 <1026920924@qq.com>
2019-08-30 15:45:43 +0800

    luYa

---
+++ luya-admin/src/components/toolsApi/getTime.vue
@@ -0,0 +1,19 @@
+<template>
+    <p style="margin-left:580px">{{time}}阅读量排行榜</p>
+</template>
+
+<script>
+export default {
+    name: 'getTime',
+    props: ['time'],
+    data() {
+        return {
+            date: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
